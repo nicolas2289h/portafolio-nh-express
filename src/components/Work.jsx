051e3d82f50fce76410136120c1f08de8e1d45fa
Nico <nicolas2289h@gmail.com>
2024-06-24 21:16:45 -0300

    agregando proyecto nocountrytube

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,7 @@
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
 import ImgTodolist from '../assets/img-work/img-todolist.png'
-import ImgClima from '../assets/img-work/img-clima.png'
+// import ImgClima from '../assets/img-work/img-clima.png'
 import ImgCifrado from '../assets/img-work/img-cifrado.png'
 import ImgWebGames from '../assets/img-work/img-webgames.png'
 import ImgReservas from '../assets/img-work/img-meriendas.png'
@@ -11,6 +11,7 @@ import ImgReservas from '../assets/img-work/img-meriendas.png'
 import ImgGestor from '../assets/img-work/img-gestion-empleados.png'
 // import ImgCalculadora from '../assets/img-work/img-calculadora.png'
 import ImgBeathub from '../assets/img-work/img-beathub.png';
+import ImgNoCountryTube from '../assets/img-work/img-nocountrytube.png'
 
 const Work = () => {
   return (
@@ -33,6 +34,17 @@ const Work = () => {
             </div>
 
             <div className='flex gap-4 lg:mt-0 mt-8 justify-end flex-wrap items-end relative'>
+              <a href='https://www.nocountrytube.site/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
+                <div className='group-hover:bg-black/85 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgNoCountryTube} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Plataforma web para compartir y visualizar videos</span>
+                </div>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-3 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #Nest.js #PostgreSQL #Bootstrap</span>
+                </div>
+              </a>
+
               <a href='https://beat-hub-app.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
                 <div className='group-hover:bg-black/85 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgBeathub} alt="" />
@@ -55,16 +67,6 @@ const Work = () => {
                 </div>
               </a>
 
-              <a href='https://todolist-react-mu.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
-                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
-                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgTodolist} alt="" />
-                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-md font-semibold'>Aplicación para Listado de tareas con LocalStorage.</span>
-                </div>
-                <div className='absolute -bottom-full left-4 group-hover:bottom-4 transition-all duration-700 z-50'>
-                  <span className='text-md font-semibold'>#React #Express #MySQL #Bootstrap</span>
-                </div>
-              </a>
             </div>
           </motion.div>
 
@@ -84,6 +86,17 @@ const Work = () => {
                 </div>
                 <div className='absolute -bottom-full left-4 group-hover:bottom-3 transition-all duration-700 z-50'>
                   <span className='text-md font-semibold'>#React #Java #SpringBoot #MySQL #Bootstrap</span>
+                </div>
+              </a>
+
+              <a href='https://todolist-react-mu.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] cursor-pointer' >
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgTodolist} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Aplicación para Listado de tareas con LocalStorage.</span>
+                </div>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-4 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #Express #MySQL #Bootstrap</span>
                 </div>
               </a>
 
@@ -120,7 +133,7 @@ const Work = () => {
                 </div>
               </a> */}
 
-              <a href='https://weather-app-seven-pearl-45.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
+              {/* <a href='https://weather-app-seven-pearl-45.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
                 <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgClima} alt="" />
                 <div className='absolute -bottom-full left-4 group-hover:bottom-14 transition-all duration-500 z-50'>
@@ -129,7 +142,7 @@ const Work = () => {
                 <div className='absolute -bottom-full left-4 group-hover:bottom-5 transition-all duration-700 z-50'>
                   <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
                 </div>
-              </a>
+              </a> */}
             </div>
           </motion.div>
 
